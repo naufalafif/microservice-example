da06f7a9828e3a2e67d3003a16247f2d1e76387a
naufal afif <naufalafif58@gmail.com>
2020-04-16 01:16:37 +0700

    init frontend

---
+++ Frontend/src/utils/request.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const service = axios.create({
+    baseURL: process.env.VUE_BASE_API ? process.env.VUE_BASE_API : 'http://localhost:3000',
+    timeout: 5000
+})
+
+service.interceptors.request.use(config => {
+        // TODO: Config Token Here
+        return config
+    },
+    error => {
+        // do something with request error
+        console.log(error) // for debug
+        return Promise.reject(error)
+    })
+
+service.interceptors.response.use(
+    response => {
+        // TODO: handle reset token
+        return response
+    },
+    error => {
+        console.log('err' + error) // for debug
+        // Message({
+        //     message: error.message,
+        //     type: 'error',
+        //     duration: 5 * 1000
+        // })
+        return Promise.reject(error)
+    }
+)
+
+export default service
---
+++ Frontend/src/views/Task.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+import { fetchList } from '@/api/task'
+
+export default {
+  name: 'Home',
+  created(){
+    fetchList().then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
