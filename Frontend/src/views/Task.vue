d6958c3728b7c7b66ae2c094cf903ec126781660
naufal afif <naufalafif58@gmail.com>
2020-04-17 17:14:31 +0700

    add frontend task layout & basic functionality

--- Frontend/src/views/Task.vue
+++ Frontend/src/views/Task.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+<el-row :gutter="20">
+  <el-col :span="12" :offset="6">
+    <el-input placeholder="Enter to save" v-model="new_task" clearable @keypress.native.enter="addTaskHandler()">
+    </el-input>
+
+    <el-table
+      :data="tasks"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="Title"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="description"
+        label="Description">
+      </el-table-column>
+    </el-table>
+
+  </el-col>
+</el-row>
+
 </template>
 
 <script>
@@ -10,12 +29,46 @@ import { fetchList } from '@/api/task'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      new_task: null,
+      /*
+      task: [{
+        id: int
+        name: str,
+        description: str,
+        done: bool
+      }]
+      */
+      tasks: [] 
+      
+    }
+  },
   created(){
     fetchList().then(res => {
-      console.log(res)
+      res.data.data.forEach(task => {
+        this.addTask(task)
+      });
     }).catch(err => {
       console.log(err)
     })
+  },
+  methods: {
+    async addTaskHandler(){
+      await this.addTask({
+        id: Math.random(),
+        name: this.new_task,
+        description: 'temporary',
+        done: false
+      })
+      this.new_task = null
+    },
+    async addTask(task){
+      let { id, name, description, done } = task
+      this.tasks.push({
+        id, name, description, done
+      })
+    }
   }
 }
 </script>
